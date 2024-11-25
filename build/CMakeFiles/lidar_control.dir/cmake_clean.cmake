79486414c7c5ccc39eb61820cd84c4b4b97cea3d
RiccieS <riccigames82@gmail.com>
2024-11-25 15:43:46 +0100

    asi ready

---
+++ build/CMakeFiles/lidar_control.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lidar_control.dir/lidar_control.cpp.o"
+  "CMakeFiles/lidar_control.dir/lidar_control.cpp.o.d"
+  "lidar_control"
+  "lidar_control.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lidar_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
